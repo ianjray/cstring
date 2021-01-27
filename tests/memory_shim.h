6e919f323dd46898c73746624cb9f15cc61dcc9d
ianjray <ianjray@me.com>
2021-01-27 21:32:06 +0200

    Initial commit
    Signed-off-by: Ian Ray <ianjray@me.com>

---
+++ tests/memory_shim.h
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+/// True if @c malloc should fail.
+extern bool g_malloc_fail;
+
+/// True if @c calloc should fail.
+extern bool g_calloc_fail;
+
+/// True if @c strdup should fail.
+extern bool g_strdup_fail;
+
+/// True if @c realloc should fail.
+extern bool g_realloc_fail;
